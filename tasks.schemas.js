3ef9121213bab8cf5a97950b7317fb90b62f67ce
KelvinchiDev <kelvinchirinos2702@gmail.com>
2025-07-04 17:48:27 -0400

    Delete tasks.schemas.js

--- tasks.schemas.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod/v4';
-
-const NAME_REGEX = /^\s*[\d\-.]?\s?.{1,77}$/;
-
-export const tasksSchema = z.object({
-  id: z.number(),
-  name: z.string().regex(NAME_REGEX, 'El nombre no es valido'),
-  complete: z.boolean(),
-});
